ba524bfdb3a82a3efabcc64f25128038e6b1c59d
hadsn <hadsn.dbf@gmail.com>
2019-09-09 14:14:23 +0900

    実績: ToDoリスト を解除

--- CalculateKeikyuLedAging/Program.cs
+++ CalculateKeikyuLedAging/Program.cs
@@ -10,6 +10,10 @@ namespace CalculateKeikyuLedAging
     {
         static void Main(string[] args)
         {
+            //時刻表取得を書く
+            //時刻表をパースして時刻と種別・行先に分離する
+            //劣化度を計算する
+            //ビットマップなどに吐く
         }
     }
 }
