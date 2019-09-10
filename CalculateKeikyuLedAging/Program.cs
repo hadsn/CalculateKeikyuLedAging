85a709d049593c93736713b2c505cadfdc85308a
hadsn <hadsn.dbf@gmail.com>
2019-09-10 20:56:33 +0900

    京急のページスクレイビングから、駅探サイトのリスト型式コピペ用に変更する前の最後

--- CalculateKeikyuLedAging/Program.cs
+++ CalculateKeikyuLedAging/Program.cs
@@ -11,6 +11,11 @@ namespace CalculateKeikyuLedAging
         static void Main(string[] args)
         {
             //時刻表取得を書く
+            Console.WriteLine("取得する時刻表のURLを入力してください\n> ");
+            String sUri = Console.ReadLine();
+            Console.WriteLine("出力するファイル名を入力してください\n> ");
+            String sOutputFileName = Console.ReadLine();
+
             //時刻表をパースして時刻と種別・行先に分離する
             //劣化度を計算する
             //ビットマップなどに吐く
